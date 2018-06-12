b873ca6a2cc6ae93c99805f7882b2c5ae5ff50f6
Jose Sicajan <sicajancoy@gmail.com>
2018-06-12 11:41:22 -0600

    assign route

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^create', views.create_task, name='create'),
     url(r'^delete', views.delete_task, name='delete'),
+    url(r'^assign', views.assign, name='assign'),
     url(r'^my', views.my_tasks, name='my_tasks'),
     url(r'^login', views.login, name='login'),
     url(r'^logout', views.logout, name='logout'),
--- tasks/views.py
+++ tasks/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 from .models import Task
@@ -45,6 +45,7 @@ def my_tasks(request):
     tasks = Task.objects.filter(assigned_to=request.user.id).order_by('-priority')
     return render(request, 'tasks/my_tasks/index.html', {'tasks': tasks})
 
+
 @login_required(login_url='/tasks/delete/')
 def delete_task(request):
     tasks = Task.objects.order_by('priority')
@@ -70,6 +71,14 @@ def create_task(request):
         new_task.save()
     tasks = Task.objects.order_by('priority')
     return render(request, 'tasks/my_tasks/create.html', {'tasks': tasks})
+
+
+@login_required(login_url='/tasks/login/')
+def assign(request):
+    if request.method == 'POST':
+        print('hello there')
+        
+    return render(request, 'tasks/my_tasks/assign.html', {'users': User.objects.all()})
 
 
 def logout(request):
